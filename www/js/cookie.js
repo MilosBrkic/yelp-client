c087f822ee82371c589a7012a8c8f3d1471e1756
MilosBrkic <36965882+MilosBrkic@users.noreply.github.com>
2020-05-13 21:12:59 +0200

    Add files via upload

---
+++ www/js/cookie.js
@@ -0,0 +1,25 @@
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
+}
+
+function removeCookie(name){
+    var d = new Date();
+    d.setTime(d.getTime() - 1);
+    document.cookie = name+'=; expires=' + d.toGMTString();
+}
---
+++ www/js/url.js
@@ -0,0 +1,2 @@
+var url ="https://yelpcampmobile.herokuapp.com/"
+//var url = "http://192.168.0.12:3000/";
